d0800ce7b9baf560e2a03365c7d5bd0b4b3e6933
Kevin <k.silvares24@gmail.com>
2023-10-02 08:54:24 +0200

    Lugar

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String args[]) {
 
 		System.out.println("Mi deporte favorito es el automovilismo");
+		System.out.println("Mi lugar favorito es León");
 	
 	}
 
